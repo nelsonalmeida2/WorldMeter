a1ac3e74ed168d615fd927a121b83ae7cea565d2
Marmelson <nelson10sonia@gmail.com>
2024-04-05 16:59:02 +0100

    Datasets for the project

--- src/pt/ulusofona/aed/deisiworldmeter/Main.java
+++ src/pt/ulusofona/aed/deisiworldmeter/Main.java
@@ -8,7 +8,6 @@ import java.util.Scanner;
 
 
 public class Main {
-
     public static ArrayList<Pais> listaPaises = new ArrayList<Pais>();
     public static ArrayList<Populacao> listaPopulacao = new ArrayList<Populacao>();
     public static ArrayList<Cidade> listaCidades = new ArrayList<Cidade>();
@@ -16,104 +15,124 @@ public class Main {
     public static InputInvalido listaPopulacaoInvalidos = new InputInvalido("populacao.csv",0,0,-1);
     public static InputInvalido listaCidadesInvalidos = new InputInvalido("cidades.csv",0,0,-1);
     public static Boolean parseFiles(File nomePasta) { // Ler os 3 ficheiros CSV, recebe o nome da pasta
-        try {
-            Scanner scanner = new Scanner(nomePasta);
 
+        try { // tenta ler o ficheiro pais senão retorna falso e não le mais nenhum
+            File filePais = new File(nomePasta,"paises.csv");
+            Scanner scannerPais = new Scanner(filePais);
             int linhaNum = 0;
             boolean primeiraLinha = true;
             boolean primeiraInvalida = true;
+            listaPaises = new ArrayList<Pais>();
+            listaPaisesInvalidos = new InputInvalido("paises.csv",0,0,-1);
+            while (scannerPais.hasNext()){
+                String linha = scannerPais.nextLine();
 
-            switch (nomePasta.getName()) {
-                case "paises.csv": // se o id for maior de 700 if
-                    listaPaises = new ArrayList<Pais>();
-                    listaPaisesInvalidos = new InputInvalido("paises.csv",0,0,-1);
-                    while (scanner.hasNext()){
-                        String linha = scanner.nextLine();
-                        if (primeiraLinha) {
-                            primeiraLinha = false;
-                        } else {
-                            String[] partes = linha.split(",");
-                            if (partes[0] == "" || partes[1] == "" || partes[2] == "" ||
-                                    partes[3] == "" || partes.length != 4) {
-                                if (primeiraInvalida) {
-                                    listaPaisesInvalidos.primeiraLinhaIncorreta = linhaNum;
-                                    primeiraInvalida = false;
-                                }
-                                linhaNum++;
-                                listaPaisesInvalidos.linhasIncorretas ++;
+                if (primeiraLinha) {
+                    primeiraLinha = false;
+                } else {
+                    String[] partes = linha.split(",");
+                    if (partes[0] == "" || partes[1] == "" || partes[2] == "" ||
+                            partes[3] == "" || partes.length != 4) {
+                        if (primeiraInvalida) {
+                            listaPaisesInvalidos.primeiraLinhaIncorreta = linhaNum;
+                            primeiraInvalida = false;
+                        }
+                        listaPaisesInvalidos.linhasIncorretas ++;
 
-                            } else {
-                                Pais pais = new Pais(Integer.parseInt(partes[0]),partes[1].toUpperCase(),partes[2].toUpperCase(),partes[3]);
-                                listaPaises.add(pais);
-                                linhaNum++;
-                                listaPaisesInvalidos.linhasCorretas ++;
+                    } else {
+                        boolean repetido = false;
+                        for (Pais pais : listaPaises) {
+                            if (pais.id == Integer.parseInt(partes[0])) {
+                                repetido = true;
+                                break; // Sai do ciclo assim que encontra repetido
                             }
                         }
-                    }
-                    break;
-
-                case "populacao.csv":
-                    listaPopulacao = new ArrayList<Populacao>();
-                    listaPopulacaoInvalidos = new InputInvalido("populacao.csv",0,0,-1);
-                    while (scanner.hasNext()){
-                        String linha = scanner.nextLine();
-                        if (primeiraLinha) {
-                            primeiraLinha = false;
+                        if (repetido) {
+                            listaPaisesInvalidos.primeiraLinhaIncorreta = linhaNum;
+                            listaPaisesInvalidos.linhasIncorretas ++;
                         } else {
-                            String[] partes = linha.split(",");
-                            if (partes[0] == "" || partes[1] == "" || partes[2] == "" ||
-                                    partes[3] == "" || partes[4] == "" || partes.length != 5 || Objects.equals(partes[1], "Medium")) {
-                                if (primeiraInvalida) {
-                                    listaPopulacaoInvalidos.primeiraLinhaIncorreta = linhaNum;
-                                    primeiraInvalida = false;
-                                }
-                                linhaNum++;
-                                listaPopulacaoInvalidos.linhasIncorretas ++;
-                            } else {
-                                Populacao populacao = new Populacao(Integer.parseInt(partes[0]),Integer.parseInt(partes[1]),
-                                        Integer.parseInt(partes[2]),Integer.parseInt(partes[3]),Double.parseDouble(partes[4]));
-                                listaPopulacao.add(populacao);
-                                linhaNum++;
-                                listaPopulacaoInvalidos.linhasCorretas ++;
-                            }
+                            Pais pais = new Pais(Integer.parseInt(partes[0]),partes[1].toUpperCase(),partes[2].toUpperCase(),partes[3]);
+                            listaPaises.add(pais);
+                            listaPaisesInvalidos.linhasCorretas ++;
                         }
                     }
-                    break;
-
-                case "cidades.csv":
-                    listaCidades = new ArrayList<Cidade>();
-                    listaCidadesInvalidos = new InputInvalido("cidades.csv",0,0,-1);
-                    while (scanner.hasNext()){
-                        String linha = scanner.nextLine();
-                        if (primeiraLinha) {
-                            primeiraLinha = false;
-                        } else {
-                            String[] partes = linha.split(",");
-                            if (partes[0] == "" || partes[1] == "" || partes[2] == "" ||
-                                    partes[3] == "" || partes[4] == "" || partes[5] == "" || partes.length != 6) {
-                                if (primeiraInvalida) {
-                                    listaCidadesInvalidos.primeiraLinhaIncorreta = linhaNum;
-                                    primeiraInvalida = false;
-                                }
-                                linhaNum ++;
-                                listaCidadesInvalidos.linhasIncorretas ++;
-                            } else {
-                                Cidade cidade = new Cidade(partes[0].toUpperCase(),partes[1],partes[2],(int)Double.parseDouble(partes[3]),
-                                        Double.parseDouble(partes[4]),Double.parseDouble(partes[5]));
-                                linhaNum++;
-                                listaCidades.add(cidade);
-                                listaCidadesInvalidos.linhasCorretas ++;
-                            }
-                        }
-                    }
-                    break;
-
-                default:
-                    return false;
+                    linhaNum++;
+                }
             }
         } catch (FileNotFoundException e) {
             return false;
         }
+
+        try { // tenta ler o cidade pais senão retorna falso e não le mais nenhum
+            File fileCidades = new File(nomePasta,"cidades.csv");
+            Scanner scannerCidades = new Scanner(fileCidades);
+            int linhaNum = 0;
+            boolean primeiraLinha = true;
+            boolean primeiraInvalida = true;
+            listaCidades = new ArrayList<Cidade>();
+            listaCidadesInvalidos = new InputInvalido("cidades.csv",0,0,-1);
+            while (scannerCidades.hasNext()){
+                String linha = scannerCidades.nextLine();
+                if (primeiraLinha) {
+                    primeiraLinha = false;
+                } else {
+                    String[] partes = linha.split(",");
+                    if (partes[0] == "" || partes[1] == "" || partes[2] == "" ||
+                            partes[3] == "" || partes[4] == "" || partes[5] == "" || partes.length != 6) {
+                        if (primeiraInvalida) {
+                            listaCidadesInvalidos.primeiraLinhaIncorreta = linhaNum;
+                            primeiraInvalida = false;
+                        }
+                        linhaNum ++;
+                        listaCidadesInvalidos.linhasIncorretas ++;
+                    } else {
+                        Cidade cidade = new Cidade(partes[0].toUpperCase(),partes[1],partes[2],(int)Double.parseDouble(partes[3]),
+                                Double.parseDouble(partes[4]),Double.parseDouble(partes[5]));
+                        linhaNum++;
+                        listaCidades.add(cidade);
+                        listaCidadesInvalidos.linhasCorretas ++;
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            return false;
+        }
+
+        try { // tenta ler o ficheiro populacao senão retorna falso
+            File filePopulacao = new File(nomePasta,"populacao.csv");
+            Scanner scannerPopulacao = new Scanner(filePopulacao);
+            int linhaNum = 0;
+            boolean primeiraLinha = true;
+            boolean primeiraInvalida = true;
+            listaPopulacao = new ArrayList<Populacao>();
+            listaPopulacaoInvalidos = new InputInvalido("populacao.csv",0,0,-1);
+            while (scannerPopulacao.hasNext()){
+                String linha = scannerPopulacao.nextLine();
+                if (primeiraLinha) {
+                    primeiraLinha = false;
+                } else {
+                    String[] partes = linha.split(",");
+                    if (partes[0] == "" || partes[1] == "" || partes[2] == "" ||
+                            partes[3] == "" || partes[4] == "" || partes.length != 5 || Objects.equals(partes[1], "Medium")) {
+                        if (primeiraInvalida) {
+                            listaPopulacaoInvalidos.primeiraLinhaIncorreta = linhaNum;
+                            primeiraInvalida = false;
+                        }
+                        linhaNum++;
+                        listaPopulacaoInvalidos.linhasIncorretas ++;
+                    } else {
+                        Populacao populacao = new Populacao(Integer.parseInt(partes[0]),Integer.parseInt(partes[1]),
+                                Integer.parseInt(partes[2]),Integer.parseInt(partes[3]),Double.parseDouble(partes[4]));
+                        listaPopulacao.add(populacao);
+                        linhaNum++;
+                        listaPopulacaoInvalidos.linhasCorretas ++;
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            return false;
+        }
+
         return true;
     }
 
@@ -139,11 +158,9 @@ public class Main {
 
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
-        boolean paisesOK = parseFiles(new File("paises.csv"));
-        boolean cidadesOK = parseFiles(new File("cidades.csv"));
-        boolean populacaoOK = parseFiles(new File("populacao.csv"));
+        boolean parseOK = parseFiles(new File("C:\\Users\\nelso\\IdeaProjects\\WorldMeter"));
         long end = System.currentTimeMillis();
-        if (!paisesOK || !cidadesOK || !populacaoOK) {
+        if (!parseOK) {
             System.out.println("Erro na leitura dos ficheiros");
         } else {
             System.out.println("Ficheiros lidos com sucesso em " + (end-start) + "ms");
